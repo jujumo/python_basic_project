b034bcb8e3315da4c96fc05cf88872c9cf5d6a19
jumo <jujumo@gmail.com>
2024-07-12 11:12:23 +0200

    use jsonargparse since it allows nested config values

--- python_basic_project/actions.py
+++ python_basic_project/actions.py
@@ -1,16 +1,14 @@
 from python_basic_project import shovel
-import configargparse
-import yaml
+from jsonargparse import ArgumentParser, ActionConfigFile
 
 
 def do_bad():
-    parser = configargparse.ArgParser(
-        config_file_parser_class=configargparse.YAMLConfigFileParser
-    )
-    parser.add('-c', '--config', required=False, is_config_file=True, help='config file path')
-    parser.add_argument("--complains", type=str, default="")
-    args = parser.parse_args()
-    shovel.dig(args.complains)
+    parser = ArgumentParser()
+    parser.add_argument('--complains.bad', type=str, default='grr !')
+    parser.add_argument('-c', '--config', action=ActionConfigFile, help='config file path')
+    config = parser.parse_args()
+
+    shovel.dig(config.complains.bad)
 
 
 if __name__ == '__main__':
